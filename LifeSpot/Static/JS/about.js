128ccf325762c53fa17c7ffd53a2ca51ad3e8727
Максим Самисько <masechk@gmail.com>
2023-09-07 20:22:57 +0300

    Unit29_LifeSpot: Added function for add users reviews into the page about

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,1 +1,21 @@
-alert(`Hello`);
+function getReview() {
+
+    let review = {};
+
+    // получаем имя пользователя
+    review["userName"] = prompt("Enter your name");
+    // получаем отзыв пользователя
+    review["userReview"] = prompt("Enter your review");
+    // сохраним дату добавления отзыва
+    review["Date"] = new Date().toLocaleString();
+
+    // запишем отзыв
+    writeReview(review);
+}
+
+function writeReview(userReview) {
+    document.getElementsByClassName("reviews")[0].innerHTML +=  '<div class="review-text">' +
+                                                                `<p> <b>${userReview["userName"]} </b>     ${userReview["Date"]}</p>` +
+                                                                `<p> ${userReview["userReview"]}</p>` + 
+                                                                '</div>';
+}
