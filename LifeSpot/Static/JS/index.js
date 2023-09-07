77982898a711c93604b21efc9b0e8b12d48665d0
Максим Самисько <masechk@gmail.com>
2023-09-07 13:40:17 +0300

    Unit29_LifeSpot: Added function filterContent() in index.js

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -8,3 +8,26 @@ else {
     alert("Bye!");
     window.location.href = "http://google.com";
 }
+
+function filterContent() {
+    // Сохраняем введённое пользователем значение и приводим его к нижнему регистру
+    let inputText = document.getElementsByTagName('input')[0].value.toLowerCase();
+    // Находим и сохраняем коллекцию блоков с видео
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i < elements.length; i++) {
+        // Помещаем в переменную описание видео
+        let videoDescription = elements[i].getElementsByTagName('h3')[0].innerText.toLowerCase();
+        console.log(videoDescription);
+
+        // Сравниваем описание с вводимым текстом
+        if (!videoDescription.includes(inputText)) {
+            // Скрываем ненужные элементы
+            elements[i].style.display = 'none';
+        }
+        else {
+            // Нужные элементы оставляем видимыми
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
