d8c634108813310d8bef07b2f5595c97cff34380
Максим Самисько <masechk@gmail.com>
2023-09-07 12:06:13 +0300

    Unit28_lifeSpot: Added script for searching video on the page and hiding usless videos

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,5 @@
+// Получим коллекцию всех элементов страницы
+let elements = document.getElementsByTagName('*');
+
+// Выведем результат в уведомление
+alert(`Count of elements in the page:  ${elements.length}`);
